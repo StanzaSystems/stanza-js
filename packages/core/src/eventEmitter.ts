a6334893e6a969b3629cd19f8c20878bb3516216
Chris Trzesniewski <chris@thisdot.co>
2023-02-28 13:11:34 +0100

    fix linting

--- packages/core/src/eventEmitter.ts
+++ packages/core/src/eventEmitter.ts
@@ -12,21 +12,43 @@ class StanzaChangeEvent extends Event {
   }
 }
 
+type StanzaChangeListener = (state: StanzaState) => unknown
+
 export interface StanzaChangeEmitter {
-  addChangeListener: (callback: (state: unknown) => void) => void
+  addChangeListener: (callback: StanzaChangeListener) => () => void
+  removeChangeListener: (callback: StanzaChangeListener) => void
   dispatchChange: (state: StanzaState) => void
 }
 
 export class StanzaChangeTarget implements StanzaChangeEmitter {
   private readonly eventTarget = new EventTarget()
+  private readonly unsubscribeMap = new Map<StanzaChangeListener, () => void>()
 
-  addChangeListener (callback: (state: StanzaState) => void): void {
-    this.eventTarget.addEventListener('stanzaStateChanged', (evt) => {
+  addChangeListener (callback: StanzaChangeListener): () => void {
+    const eventListener = (evt: unknown): void => {
       if (!(evt instanceof StanzaChangeEvent)) {
         return
       }
       callback(evt.detail)
-    })
+    }
+
+    const unsubscribe = (): void => {
+      this.unsubscribeMap.delete(callback)
+      this.eventTarget.removeEventListener('stanzaStateChanged', eventListener)
+    }
+
+    this.unsubscribeMap.set(callback, unsubscribe)
+    this.eventTarget.addEventListener('stanzaStateChanged', eventListener)
+
+    return unsubscribe
+  }
+
+  removeChangeListener (callback: StanzaChangeListener): void {
+    const unsubscribe = this.unsubscribeMap.get(callback)
+
+    if (typeof unsubscribe === 'function') {
+      unsubscribe()
+    }
   }
 
   dispatchChange (state: StanzaState): void {
