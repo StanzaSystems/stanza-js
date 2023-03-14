ed9a37854efa1009b3ed2cc61f685d02ec81f3f1
Chris Trześniewski <125868670+ktrz-sre-is@users.noreply.github.com>
2023-03-14 09:41:21 +0100

    feat: 18 add ETag to polling requests (#32)
    * store eTag for polling requests
    * send `If-None-Match` header if eTag available
    * add proxy to hub in Next example to allow access to eTag header

--- samples/simple-next/next.config.js
+++ samples/simple-next/next.config.js
@@ -6,7 +6,19 @@ const withTM = require('next-transpile-modules')([
 
 const COOKIE_PREFIX = process.env.NODE_ENV === 'development' ? '__Dev-' : '__Host-'
 
+const rewrites = () => {
+  return {
+    fallback: [
+      {
+        source: '/api/hub/:path*',
+        destination: 'https://hub.dev.getstanza.dev/:path*',
+      },
+    ]
+  }
+}
+
 module.exports = withTM({
+  rewrites,
   reactStrictMode: true,
   publicRuntimeConfig: {
     stanzaEnablementNumberCookieName: `${COOKIE_PREFIX}stanza-enablement-number`,
