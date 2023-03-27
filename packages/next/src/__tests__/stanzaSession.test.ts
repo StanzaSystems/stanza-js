2f4a34c194d496d2af92d30b20cd66b393f03828
Chris Trzesniewski <chris@thisdot.co>
2023-03-27 12:20:46 +0200

    feat: add Next middleware support
    * add unit tests

--- packages/next/src/__tests__/stanzaSession.test.ts
+++ packages/next/src/__tests__/stanzaSession.test.ts
@@ -1,20 +1,28 @@
 import { type NextApiRequest, type NextApiResponse } from 'next'
+import { type NextRequest, NextResponse } from 'next/server'
 import { describe, expect, it, vi } from 'vitest'
 import { stanzaSession } from '../stanzaSession'
 
-const createMockRequest = (cookies: Partial<Record<string, string>> = {}, headers: Partial<Record<string, string | string[] | undefined>> = {}): NextApiRequest => ({
+const createMockApiRequest = (cookies: Partial<Record<string, string>> = {}, headers: Partial<Record<string, string | string[] | undefined>> = {}): NextApiRequest => ({
   cookies,
   headers
 } as any as NextApiRequest)
 
-// const createMockResponse = ({
-//   getHeader = () => undefined, setHeader = function () {
-//     return this
-//   } as (this: NextApiResponse) => NextApiResponse
-// }: Partial<Pick<NextApiResponse, 'getHeader' | 'setHeader'>> = {}): NextApiResponse => ({
-//   getHeader,
-//   setHeader
-// } as any as NextApiResponse)
+const createMockNextRequest = (cookies: Record<string, string> = {}, headers: Record<string, string> = {}): NextRequest => ({
+  cookies: {
+    get: (key: string) => ({ value: cookies[key], key })
+  },
+  headers: new Headers(headers)
+} as any as NextRequest)
+
+const createMockApiResponse = ({
+  getHeader = () => undefined, setHeader = function () {
+    return this
+  } as (this: NextApiResponse) => NextApiResponse
+}: Partial<Pick<NextApiResponse, 'getHeader' | 'setHeader'>> = {}): NextApiResponse => ({
+  getHeader,
+  setHeader
+} as any as NextApiResponse)
 
 describe('stanzaSession', () => {
   it('should not throw on creation', () => {
@@ -27,53 +35,224 @@ describe('stanzaSession', () => {
     expect(typeof session.getEnablementNumber).toBe('function')
   })
 
-  it('should return existing value if already exists on request', async () => {
-    const req = createMockRequest({
-      'stanza-enablement-number': '50'
+  describe('getEnablementNumber', function () {
+    it('should return existing value if already exists on request', async () => {
+      const req = createMockApiRequest({
+        'stanza-enablement-number': '50'
+      })
+      const session = stanzaSession()
+      await expect(session.getEnablementNumber(req)).resolves.toBe(50)
     })
-    const session = stanzaSession({
-      generateEnablementNumber: () => 99
+
+    it('should return undefined if cookie doesn\'t exists on request', async () => {
+      const req = createMockApiRequest()
+      const session = stanzaSession()
+      await session.getEnablementNumber(req)
+
+      const enablementNumber = await session.getEnablementNumber(req)
+
+      expect(enablementNumber).toBeUndefined()
     })
-    await expect(session.getEnablementNumber(req)).resolves.toBe(50)
+
+    it('should return undefined if if cookie contains invalid value', async () => {
+      const req = createMockApiRequest({
+        'stanza-enablement-number': 'invalidValue'
+      })
+      const session = stanzaSession()
+
+      const enablementNumber = await session.getEnablementNumber(req)
+
+      expect(enablementNumber).toBeUndefined()
+    })
   })
 
-  it('should not set new cookie if already exists on request', async () => {
-    const req = createMockRequest({
-      'stanza-enablement-number': '50'
-    })
-    const setHeader: NextApiResponse['setHeader'] = vi.fn(function () {
-      return this
-    } as (this: NextApiResponse) => NextApiResponse)
-    const session = stanzaSession({
-      generateEnablementNumber: () => 99
-    })
-    await session.getEnablementNumber(req)
+  describe('middleware', function () {
+    it('should set x-stanza-enablement-number header with a cookie on a request', async () => {
+      const nextResponseSpy = vi.spyOn(NextResponse, 'next')
+      const req = createMockNextRequest({
+        'stanza-enablement-number': '50'
+      })
+      const session = stanzaSession({
+        generateEnablementNumber: () => 99
+      })
 
-    expect(setHeader).not.toHaveBeenCalled()
+      await session.middleware(req)
+
+      expect(nextResponseSpy).toHaveBeenCalledTimes(1)
+      expect(nextResponseSpy).toHaveBeenCalledWith({
+        request: {
+          headers: new Headers({
+            'x-stanza-enablement-number': '50'
+          })
+        }
+      })
+    })
+
+    it('should not generate new enablement number if it does exist on request', async () => {
+      const req = createMockNextRequest({
+        'stanza-enablement-number': '50'
+      })
+      const generateEnablementNumber = vi.fn(() => 99)
+      const session = stanzaSession({
+        generateEnablementNumber
+      })
+      await session.middleware(req)
+
+      expect(generateEnablementNumber).not.toHaveBeenCalled()
+    })
+
+    it('should not set new cookie if already exists on request', async () => {
+      const req = createMockNextRequest({
+        'stanza-enablement-number': '50'
+      })
+      const session = stanzaSession({
+        generateEnablementNumber: () => 99
+      })
+      const res = await session.middleware(req)
+
+      expect(res.cookies.get('stanza-enablement-number')?.value).toBeUndefined()
+    })
+
+    it('should generate new random enablement number if it does not exist on request', async () => {
+      const req = createMockNextRequest()
+      const generateEnablementNumber = vi.spyOn(Math, 'random')
+      generateEnablementNumber.mockImplementationOnce(() => 0.5)
+      const session = stanzaSession()
+      const res = await session.middleware(req)
+
+      expect(generateEnablementNumber).toHaveBeenCalledTimes(1)
+      expect(res.cookies.get('stanza-enablement-number')?.value).toBe('49')
+    })
+
+    it('should generate new enablement number if it does not exist on request', async () => {
+      const req = createMockNextRequest()
+      const generateEnablementNumber = vi.fn(() => 99)
+      const session = stanzaSession({
+        generateEnablementNumber
+      })
+      await session.middleware(req)
+
+      expect(generateEnablementNumber).toHaveBeenCalledTimes(1)
+    })
+
+    it('should set response cookie if enablement number does not exist on request', async () => {
+      const req = createMockNextRequest()
+      const session = stanzaSession({
+        generateEnablementNumber: () => 99
+      })
+      const res = await session.middleware(req)
+
+      expect(res.cookies.get('stanza-enablement-number')?.value).toBe('99')
+    })
+
+    it('should generate new enablement number if it is not valid on request', async () => {
+      const req = createMockNextRequest({
+        'stanza-enablement-number': 'invalidNumber'
+      })
+      const generateEnablementNumber = vi.fn(() => 99)
+      const session = stanzaSession({
+        generateEnablementNumber
+      })
+      await session.middleware(req)
+
+      expect(generateEnablementNumber).toHaveBeenCalledTimes(1)
+    })
+
+    it('should set response cookie if enablement number is not valid on request', async () => {
+      const req = createMockNextRequest({
+        'stanza-enablement-number': 'invalidNumber'
+      })
+      const session = stanzaSession({
+        generateEnablementNumber: () => 99
+      })
+      const res = await session.middleware(req)
+
+      expect(res.cookies.get('stanza-enablement-number')?.value).toBe('99')
+    })
   })
 
-  it('should return undefined if cookie doesn\'t exists on request', async () => {
-    const req = createMockRequest()
-    const session = stanzaSession({
-      generateEnablementNumber: () => 99
-    })
-    await session.getEnablementNumber(req)
+  describe('withStanzaSession', function () {
+    it('should add existing enablement number to a session', function () {
+      const req = createMockApiRequest({}, {
+        'x-stanza-enablement-number': '50'
+      })
+      const res = createMockApiResponse()
 
-    const enablementNumber = await session.getEnablementNumber(req)
+      const session = stanzaSession()
 
-    expect(enablementNumber).toBeUndefined()
-  })
+      session.withStanzaSession((req, _) => {
+        expect(req.stanzaSession.enablementNumber).toEqual(50)
+      })(req, res)
 
-  it('should return undefined if if cookie contains invalid value', async () => {
-    const req = createMockRequest({
-      'stanza-enablement-number': 'invalidValue'
-    })
-    const session = stanzaSession({
-      generateEnablementNumber: () => 99
+      expect.assertions(1)
     })
 
-    const enablementNumber = await session.getEnablementNumber(req)
+    it('should add existing enablement number to a session from a cookie', function () {
+      const req = createMockApiRequest({
+        'stanza-enablement-number': '50'
+      })
+      const res = createMockApiResponse()
 
-    expect(enablementNumber).toBeUndefined()
+      const session = stanzaSession()
+
+      session.withStanzaSession((req, _) => {
+        expect(req.stanzaSession.enablementNumber).toEqual(50)
+      })(req, res)
+
+      expect.assertions(1)
+    })
+
+    it('should choose enablement number to a header over a cookie', function () {
+      const req = createMockApiRequest({
+        'stanza-enablement-number': '50'
+      }, {
+        'x-stanza-enablement-number': '60'
+      })
+      const res = createMockApiResponse()
+
+      const session = stanzaSession()
+
+      session.withStanzaSession((req, _) => {
+        expect(req.stanzaSession.enablementNumber).toEqual(60)
+      })(req, res)
+
+      expect.assertions(1)
+    })
+
+    it('should generate new enablement number if it does not exist on a request', function () {
+      const req = createMockApiRequest({})
+      const res = createMockApiResponse()
+
+      const generateEnablementNumber = vi.fn(() => 99)
+      const session = stanzaSession({
+        generateEnablementNumber
+      })
+
+      session.withStanzaSession((req, _) => {
+        expect(generateEnablementNumber).toHaveBeenCalledOnce()
+        expect(req.stanzaSession.enablementNumber).toEqual(99)
+      })(req, res)
+
+      expect.assertions(2)
+    })
+
+    it('should generate new enablement number request contains invalid value', function () {
+      const req = createMockApiRequest({
+        'x-stanza-enablement-number': 'invalidValue'
+      })
+      const res = createMockApiResponse()
+
+      const generateEnablementNumber = vi.fn(() => 99)
+      const session = stanzaSession({
+        generateEnablementNumber
+      })
+
+      session.withStanzaSession((req, _) => {
+        expect(generateEnablementNumber).toHaveBeenCalledOnce()
+        expect(req.stanzaSession.enablementNumber).toEqual(99)
+      })(req, res)
+
+      expect.assertions(2)
+    })
   })
 })
