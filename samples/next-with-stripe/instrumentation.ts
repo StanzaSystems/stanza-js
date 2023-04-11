1559698297640e54ec75704e635d4087410fc963
Chris Trzesniewski <chris@thisdot.co>
2023-04-11 10:44:06 +0200

    remove unnecessary try-catch

--- samples/next-with-stripe/instrumentation.ts
+++ samples/next-with-stripe/instrumentation.ts
@@ -8,17 +8,13 @@ export async function register () {
     return
   }
 
-  try {
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const { init } = require('@getstanza/node')
-    await init({
-      hubUrl: 'http:/localhost:9010',
-      apiKey: 'valid-api-key',
-      serviceName: 'DemoService',
-      serviceRelease: '1',
-      environment: 'local'
-    })
-  } catch (e) {
-    console.error('failed', e)
-  }
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const { init } = require('@getstanza/node')
+  await init({
+    hubUrl: 'http:/localhost:9010',
+    apiKey: 'valid-api-key',
+    serviceName: 'DemoService',
+    serviceRelease: '1',
+    environment: 'local'
+  })
 }
