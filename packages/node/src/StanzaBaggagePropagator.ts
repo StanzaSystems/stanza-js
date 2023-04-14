d4c21e434402ccaba8beecb9d62f79b4cd128d47
Chris Trześniewski <125868670+ktrz-sre-is@users.noreply.github.com>
2023-04-14 12:45:15 +0200

    feat: 45 add Stanza baggage propagation (#60)
    * propagate Stanza baggage into Jaeger and Datadog specific baggage keys
    * add unit tests

---
+++ packages/node/src/StanzaBaggagePropagator.ts
@@ -0,0 +1,30 @@
+import { type Context, propagation, type TextMapGetter, type TextMapSetter } from '@opentelemetry/api'
+import { W3CBaggagePropagator } from '@opentelemetry/core'
+import { getAllStanzaBaggageEntries, getStanzaBaggageKeys } from './stanzaBaggageKey'
+
+export const enrichContextBaggage = (context: Context): Context => {
+  const baggage = propagation.getBaggage(context)
+
+  if (baggage === undefined) {
+    return context
+  }
+
+  const stanzaEntries = getAllStanzaBaggageEntries(baggage)
+
+  const newBaggage = stanzaEntries
+    .map(({ key: stanzaKey, entry }) => getStanzaBaggageKeys(stanzaKey).map(key => ({ key, entry })))
+    .flat()
+    .reduce((currentBaggage, { key, entry }) => currentBaggage.setEntry(key, entry), baggage)
+
+  return propagation.setBaggage(context, newBaggage)
+}
+
+export class StanzaBaggagePropagator extends W3CBaggagePropagator {
+  override inject (context: Context, carrier: unknown, setter: TextMapSetter): void {
+    super.inject(enrichContextBaggage(context), carrier, setter)
+  }
+
+  override extract (context: Context, carrier: unknown, getter: TextMapGetter): Context {
+    return enrichContextBaggage(super.extract(context, carrier, getter))
+  }
+}
---
+++ packages/node/types/vitest.d.ts
@@ -0,0 +1,18 @@
+import { type BaggageEntry } from '@opentelemetry/api'
+import { type SyncExpectationResult } from '@vitest/expect'
+
+interface CustomMatchers {
+  // eslint-disable-next-line @typescript-eslint/method-signature-style
+  toHaveBaggage(expected: Record<string, BaggageEntry>): SyncExpectationResult
+}
+
+declare global {
+  namespace Vi {
+    interface Assertion extends CustomMatchers {
+    }
+
+    interface AsymmetricMatchersContaining extends CustomMatchers {
+    }
+
+  }
+}
