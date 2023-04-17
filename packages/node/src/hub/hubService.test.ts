8c480fc94209ab3fcf0619b8863dd27bed04fdbf
Chris Trzesniewski <chris@thisdot.co>
2023-04-17 18:20:13 +0200

    feat: 44 add decorator code wrapper
    * add unit tests

--- packages/node/src/hub/hubService.test.ts
+++ packages/node/src/hub/hubService.test.ts
@@ -1,4 +1,5 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { type DecoratorConfigResponse } from './model/decoratorConfigResponse'
 import { type ServiceConfigResponse } from './model/serviceConfigResponse'
 import { createHubService } from './hubService'
 
@@ -28,10 +29,9 @@ describe('hubService', async () => {
       serviceRelease: '1',
       environment: 'test'
     })
-    it('should call fetch with proper params', async () => {
-      await fetchServiceConfig({
 
-      })
+    it('should call fetch with proper params', async () => {
+      await fetchServiceConfig()
 
       expect(fetchMock).toHaveBeenCalledOnce()
       expect(fetchMock).toHaveBeenCalledWith(
@@ -143,6 +143,129 @@ describe('hubService', async () => {
       })
 
       void fetchServiceConfig().catch((e) => {
+        expect(e).toEqual(new Error('Hub request timed out'))
+      })
+
+      await vi.advanceTimersByTimeAsync(1000)
+      expect.assertions(1)
+    })
+  })
+
+  describe('fetchDecoratorConfig', function () {
+    const { fetchDecoratorConfig } = createHubService({
+      hubUrl: 'https://url.to.hub',
+      apiKey: 'valid-api-key',
+      serviceName: 'TestService',
+      serviceRelease: '1',
+      environment: 'test'
+    })
+
+    it('should call fetch with proper params', async () => {
+      await fetchDecoratorConfig({
+        decorator: 'test-decorator'
+      })
+
+      expect(fetchMock).toHaveBeenCalledOnce()
+      expect(fetchMock).toHaveBeenCalledWith(
+        new URL('https://url.to.hub/v1/config/decorator?decorator=test-decorator&service.name=TestService&service.release=1&service.environment=test'),
+        {
+          headers: {
+            'X-Stanza-Key': 'valid-api-key'
+          }
+        }
+      )
+    })
+
+    it('should call fetch with proper params - including lastVersionSeen', async () => {
+      await fetchDecoratorConfig({
+        decorator: 'test-decorator',
+        lastVersionSeen: '123'
+      })
+
+      expect(fetchMock).toHaveBeenCalledOnce()
+      expect(fetchMock).toHaveBeenCalledWith(
+        new URL('https://url.to.hub/v1/config/decorator?decorator=test-decorator&service.name=TestService&service.release=1&service.environment=test&versionSeen=123'),
+        {
+          headers: {
+            'X-Stanza-Key': 'valid-api-key'
+          }
+        }
+      )
+    })
+
+    it('should return null if invalid data returned', async () => {
+      const result = await fetchDecoratorConfig({ decorator: 'test-decorator' })
+
+      expect(result).toBeNull()
+    })
+
+    it('should return null if configDataSent is false', async () => {
+      fetchMock.mockImplementation(async () => {
+        return {
+          json: async () => ({
+            version: '1',
+            configDataSent: false
+          } satisfies ServiceConfigResponse)
+        }
+      })
+
+      const result = await fetchDecoratorConfig({ decorator: 'test-decorator' })
+
+      expect(result).toBeNull()
+    })
+
+    it('should return config data if configDataSent is true', async () => {
+      fetchMock.mockImplementation(async () => {
+        return {
+          json: async () => ({
+            version: '1',
+            configDataSent: true,
+            config: {
+              decorator: 'test-decorator',
+              checkQuota: true,
+              environment: 'test',
+              quotaTags: [],
+              strictSynchronousQuota: true,
+              validateIngressTokens: false,
+              traceConfig: {
+                collectorUrl: 'https://url.to.trace.collector',
+                collectorKey: 'trace-collector-key',
+                overrides: [],
+                sampleRateDefault: 0.5
+              }
+            }
+          } satisfies DecoratorConfigResponse)
+        }
+      })
+
+      const result = await fetchDecoratorConfig({ decorator: 'test-decorator' })
+
+      expect(result).toEqual({
+        version: '1',
+        config: {
+          decorator: 'test-decorator',
+          checkQuota: true,
+          environment: 'test',
+          quotaTags: [],
+          strictSynchronousQuota: true,
+          validateIngressTokens: false,
+          traceConfig: {
+            collectorUrl: 'https://url.to.trace.collector',
+            collectorKey: 'trace-collector-key',
+            overrides: [],
+            sampleRateDefault: 0.5
+          }
+        }
+      })
+    })
+
+    it('should timeout if fetch runs too long', async () => {
+      vi.useFakeTimers()
+      fetchMock.mockImplementation(async () => {
+        return new Promise(() => {})
+      })
+
+      void fetchDecoratorConfig({ decorator: 'test-decorator' }).catch((e) => {
         expect(e).toEqual(new Error('Hub request timed out'))
       })
 
