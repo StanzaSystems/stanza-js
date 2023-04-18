2104cdc7fbbe70e79c049c51d6ee3997666a5d06
Chris Trześniewski <125868670+ktrz-sre-is@users.noreply.github.com>
2023-04-18 09:35:20 +0200

    fix: remove temporary testing call to ping endpoint (#66)

--- samples/next-with-stripe/utils/stripe-api.ts
+++ samples/next-with-stripe/utils/stripe-api.ts
@@ -36,9 +36,6 @@ const createStripeAPI = async (key: string): Promise<StripeAPI> => {
 
       url.searchParams.append('expand[]', 'data.default_price')
 
-      // TODO: temporary call for testing
-      await fetch('http://localhost:3001/ping')
-
       const response = await fetch(url, {
         headers: {
           Authorization: `Bearer ${key}`
