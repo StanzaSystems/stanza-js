fed373057f3cde885874d39007512150282dd8a1
Chris Trzesniewski <chris@thisdot.co>
2023-04-19 12:23:31 +0200

    extract addPriorityBoostToContextBaggage into separate file

--- packages/node/src/propagation/StanzaPriorityBoostPropagator.ts
+++ packages/node/src/propagation/StanzaPriorityBoostPropagator.ts
@@ -1,32 +1,6 @@
-import { type Baggage, type Context, propagation, type TextMapGetter, type TextMapSetter } from '@opentelemetry/api'
+import { type Context, type TextMapGetter, type TextMapSetter } from '@opentelemetry/api'
 import { W3CBaggagePropagator } from '@opentelemetry/core'
-import { getStanzaBaggageEntry } from '../baggage/getStanzaBaggageEntry'
-import { getStanzaBaggageKeys } from '../baggage/getStanzaBaggageKeys'
-import { addPriorityBoostToContext } from '../context/addPriorityBoostToContext'
-import { stanzaPriorityBoostContextKey } from '../context/stanzaPriorityBoostContextKey'
-
-const getStanzaPriorityBoost = (baggage: Baggage) => {
-  const baggageMaybePriorityBoost = parseInt(getStanzaBaggageEntry('stz-boost', baggage)?.value ?? '')
-
-  return !isNaN(baggageMaybePriorityBoost) ? baggageMaybePriorityBoost : 0
-}
-
-export const addPriorityBoostToContextBaggage = (context: Context): Context => {
-  const baggage = propagation.getBaggage(context) ?? propagation.createBaggage()
-
-  const contextWithTotalPriorityBoost = addPriorityBoostToContext(getStanzaPriorityBoost(baggage))(context)
-
-  const totalPriorityBoost = contextWithTotalPriorityBoost.getValue(stanzaPriorityBoostContextKey)
-
-  const boostStanzaBaggageKeys = getStanzaBaggageKeys('stz-boost')
-
-  const newBaggage = typeof (totalPriorityBoost) !== 'number' || totalPriorityBoost === 0
-    ? baggage.removeEntries(...boostStanzaBaggageKeys)
-    : boostStanzaBaggageKeys
-      .reduce((currentBaggage, key) => currentBaggage.setEntry(key, { value: totalPriorityBoost.toFixed(0) }), baggage)
-
-  return propagation.setBaggage(context, newBaggage)
-}
+import { addPriorityBoostToContextBaggage } from '../baggage/addPriorityBoostToContextBaggage'
 
 export class StanzaPriorityBoostPropagator extends W3CBaggagePropagator {
   override inject (context: Context, carrier: unknown, setter: TextMapSetter): void {
