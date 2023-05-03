985c5e4a8d350b8c9a1620c0aa85ff3804684fba
Chris Trzesniewski <chris@thisdot.co>
2023-05-03 14:49:10 +0200

    feat: 67 support validateIngressToken decorator
    * extract common propagator logic into HeaderContextPropagator

--- packages/node/src/propagation/StanzaApiKeyPropagator.ts
+++ packages/node/src/propagation/StanzaApiKeyPropagator.ts
@@ -1,28 +1,11 @@
-import { type Context, type TextMapGetter, type TextMapPropagator, type TextMapSetter } from '@opentelemetry/api'
+import { type TextMapPropagator } from '@opentelemetry/api'
 import { stanzaApiKeyContextKey } from '../context/stanzaApiKeyContextKey'
+import { HeaderContextPropagator } from './HeaderContextPropagator'
 
 const headerApiKey = 'x-stanza-key'
 
-export class StanzaApiKeyPropagator implements TextMapPropagator {
-  inject (context: Context, carrier: unknown, setter: TextMapSetter): void {
-    const stanzaApiKey = context.getValue(stanzaApiKeyContextKey)
-    if (typeof (stanzaApiKey) === 'string' && stanzaApiKey !== '') {
-      setter.set(carrier, headerApiKey, stanzaApiKey)
-    }
-  }
-
-  extract (context: Context, carrier: unknown, getter: TextMapGetter): Context {
-    const headerValue = getter.get(carrier, headerApiKey)
-    const apiKey = Array.isArray(headerValue) && headerValue.length > 0
-      ? headerValue[0]
-      : typeof (headerValue) === 'string' && headerValue !== ''
-        ? headerValue
-        : undefined
-
-    return apiKey !== undefined ? context.setValue(stanzaApiKeyContextKey, apiKey) : context
-  }
-
-  fields (): string[] {
-    return [headerApiKey]
+export class StanzaApiKeyPropagator extends HeaderContextPropagator implements TextMapPropagator {
+  constructor () {
+    super(headerApiKey, stanzaApiKeyContextKey)
   }
 }
--- packages/node/src/propagation/StanzaTokenPropagator.ts
+++ packages/node/src/propagation/StanzaTokenPropagator.ts
@@ -1,28 +1,11 @@
-import { type Context, type TextMapGetter, type TextMapPropagator, type TextMapSetter } from '@opentelemetry/api'
+import { type TextMapPropagator } from '@opentelemetry/api'
 import { stanzaTokenContextKey } from '../context/stanzaTokenContextKey'
+import { HeaderContextPropagator } from './HeaderContextPropagator'
 
 const headerStanzaToken = 'x-stanza-token'
 
-export class StanzaTokenPropagator implements TextMapPropagator {
-  inject (context: Context, carrier: unknown, setter: TextMapSetter): void {
-    const stanzaToken = context.getValue(stanzaTokenContextKey)
-    if (typeof (stanzaToken) === 'string' && stanzaToken !== '') {
-      setter.set(carrier, headerStanzaToken, stanzaToken)
-    }
-  }
-
-  extract (context: Context, carrier: unknown, getter: TextMapGetter): Context {
-    const headerValue = getter.get(carrier, headerStanzaToken)
-    const token = Array.isArray(headerValue) && headerValue.length > 0
-      ? headerValue[0]
-      : typeof (headerValue) === 'string' && headerValue !== ''
-        ? headerValue
-        : undefined
-
-    return token !== undefined ? context.setValue(stanzaTokenContextKey, token) : context
-  }
-
-  fields (): string[] {
-    return [headerStanzaToken]
+export class StanzaTokenPropagator extends HeaderContextPropagator implements TextMapPropagator {
+  constructor () {
+    super(headerStanzaToken, stanzaTokenContextKey)
   }
 }
