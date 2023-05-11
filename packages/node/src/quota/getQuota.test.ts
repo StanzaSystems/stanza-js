6f7580fc36b451849461c1bf93413d5923f866ce
Chris Trzesniewski <chris@thisdot.co>
2023-05-11 11:23:20 +0200

    feat: 48 implement GetTokenLease
    * fix tests

--- packages/node/src/quota/getQuota.test.ts
+++ packages/node/src/quota/getQuota.test.ts
@@ -1,9 +1,32 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { mockHubService } from '../__tests__/mocks/mockHubService'
 import { getQuota } from './getQuota'
+import { type TokenStore } from './tokenStore'
+import { createTokenStore } from './createTokenStore'
+
+vi.mock('../global/tokenStore', () => {
+  return {
+    tokenStore: {
+      getToken: async (...args) => mockTokenStore.getToken(...args),
+      markTokenAsConsumed: (...args) => { mockTokenStore.markTokenAsConsumed(...args) }
+    } satisfies TokenStore
+  }
+})
+
+const mockTokenStore = {
+  getToken: vi.fn(),
+  markTokenAsConsumed: vi.fn()
+} satisfies TokenStore
 
 beforeEach(() => {
+  const tokenStore = createTokenStore()
+
   mockHubService.reset()
+  mockTokenStore.getToken.mockReset()
+  mockTokenStore.markTokenAsConsumed.mockReset()
+
+  mockTokenStore.getToken.mockImplementation(tokenStore.getToken)
+  mockTokenStore.markTokenAsConsumed.mockImplementation(tokenStore.markTokenAsConsumed)
 })
 describe('getQuota', function () {
   describe('strictSynchronousQuota', () => {
@@ -33,10 +56,12 @@ describe('getQuota', function () {
     })
 
     it('should return null if getting quota times out', async function () {
-      await expect(getQuota({ decorator: 'testDecorator', isStrictSynchronousQuota: false })).resolves.toEqual(null)
+      const getQuotaPromise = getQuota({ decorator: 'testDecorator', isStrictSynchronousQuota: false })
+      await vi.advanceTimersByTimeAsync(1000)
+      await expect(getQuotaPromise).resolves.toEqual(null)
     })
 
-    it.only('should get quota if hubService returns valid token', async () => {
+    it('should get quota if hubService returns valid token', async () => {
       mockHubService.getTokenLease.mockImplementationOnce(async () => {
         return Promise.resolve({ granted: true, leases: [{ token: 'testToken', expiresAt: 500, feature: 'testFeature', priorityBoost: 0 }] })
       })
