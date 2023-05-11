ddf8be1eaf0be6a63305daef22e2bdd65958e46b
Chris Trzesniewski <chris@thisdot.co>
2023-05-11 10:29:41 +0200

    feat: 48 implement GetTokenLease
    * mark tokens as consumed

--- packages/node/src/quota/getQuota.ts
+++ packages/node/src/quota/getQuota.ts
@@ -12,19 +12,18 @@ interface GetQuotaOptions {
   priorityBoost?: number
 }
 export const getQuota = async (options: GetQuotaOptions): Promise<StanzaToken | null> => {
-  // TODO
-  // if (decoratorConfigFromContext.isStrict()) {
-  //   // call hub
-  // } else {
-  //   // call store
-  // }
   try {
     return await withTimeout(
       CHECK_QUOTA_TIMEOUT,
       'Check quota timed out',
       options.isStrictSynchronousQuota
         ? hubService.getToken(options)
-        : tokenStore.getToken(options)
+        : tokenStore.getToken(options).then(tokenInfo => {
+          if (tokenInfo?.granted === true) {
+            tokenStore.markTokenAsConsumed(tokenInfo.token)
+          }
+          return tokenInfo
+        })
     )
   } catch (e) {
     console.warn('Failed to fetch the token:', e instanceof Error ? e.message : e)
