a1ae74c84590a328b07feea9672ad046243a68d8
Maggie Johnson-Pint <maggiepint@gmail.com>
2023-05-22 14:42:04 -0700

    log api url

--- samples/next-with-stripe/instrumentation.ts
+++ samples/next-with-stripe/instrumentation.ts
@@ -2,6 +2,7 @@ export async function register () {
   if (process.env.NEXT_RUNTIME === 'nodejs') {
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const { init } = require('@getstanza/node')
+    console.log(`Stanza URL for insturmentation: ${process.env.NEXT_PUBLIC_STANZA_API}`)
     await init({
       hubUrl: process.env.NEXT_PUBLIC_STANZA_API ?? 'https://hub.demo.getstanza.io',
       apiKey: 'valid-api-key',
