76e90da95fd53f408193f7b85c544deda6beea2d
Maggie Johnson-Pint <maggiepint@gmail.com>
2023-05-22 15:24:32 -0700

    add more monitoring

---
+++ samples/next-with-stripe/sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://527e1ae7b45f4449a9b029c62d8b2e64@o4503916240240640.ingest.sentry.io/4505229878165504",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
