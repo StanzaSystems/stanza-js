19843bb23aa4486d5e8c575d272ee7f5b2a3228a
Chris Trzesniewski <chris@thisdot.co>
2023-05-29 14:42:32 +0200

    feat: 51 - hierarchical decorators and tags
    * extract ingress token validation and quota checking into separate files

---
+++ packages/node/src/decorator/guard/quotaChecker.ts
@@ -0,0 +1,30 @@
+import { getDecoratorConfig } from '../../global/decoratorConfig'
+import { getQuota } from '../../quota/getQuota'
+import { StanzaDecoratorError } from '../stanzaDecoratorError'
+
+export interface QuotaCheckerOptions {
+  decorator: string
+  feature?: string
+  priorityBoost?: number
+}
+
+export const initQuotaChecker = (options: QuotaCheckerOptions) => {
+  return { shouldCheckQuota, checkQuota }
+
+  function shouldCheckQuota (): boolean {
+    const decoratorConfig = getDecoratorConfig(options.decorator)
+
+    return decoratorConfig?.config?.checkQuota === true
+  }
+
+  async function checkQuota (): Promise<{ type: 'TOKEN_GRANTED', token: string } | null> {
+    const token = await getQuota({
+      ...options
+    })
+    if (token?.granted === false) {
+      throw new StanzaDecoratorError('NoQuota', 'Decorator can not be executed')
+    }
+
+    return token?.granted ? { type: 'TOKEN_GRANTED', token: token.token } : null
+  }
+}
