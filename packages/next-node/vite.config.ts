1c26ae9a9bda71e7f524f1b494a988142f2ed491
Chris Trzesniewski <chris@thisdot.co>
2023-06-08 00:24:50 +0200

    * exclude gen dir from coverage

--- packages/next-node/vite.config.ts
+++ packages/next-node/vite.config.ts
@@ -29,7 +29,22 @@ export default defineConfig({
     include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
     coverage: {
       reporter: [['lcov', { projectRoot: '.' }]],
-      reportsDirectory: '../../coverage/packages/next-node'
+      reportsDirectory: '../../coverage/packages/next-node',
+      exclude: [
+        'packages/node/gen/**',
+        'coverage/**',
+        'dist/**',
+        'packages/*/test{,s}/**',
+        '**/*.d.ts',
+        'cypress/**',
+        'test{,s}/**',
+        'test{,-*}.{js,cjs,mjs,ts,tsx,jsx}',
+        '**/*{.,-}test.{js,cjs,mjs,ts,tsx,jsx}',
+        '**/*{.,-}spec.{js,cjs,mjs,ts,tsx,jsx}',
+        '**/__tests__/**',
+        '**/{karma,rollup,webpack,vite,vitest,jest,ava,babel,nyc,cypress,tsup,build}.config.*',
+        '**/.{eslint,mocha,prettier}rc.{js,cjs,yml}'
+      ]
     }
   }
 })
