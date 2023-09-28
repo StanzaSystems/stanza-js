ffb824abac329d61e4bac9fdb888d454e10853e8
Chris Trzesniewski <chris@thisdot.co>
2023-09-28 15:48:19 +0200

    chore: separate API and Browser keys in Next examples

--- samples/next-with-stripe/stanzaConfig.ts
+++ samples/next-with-stripe/stanzaConfig.ts
@@ -1,16 +1,25 @@
 import type { StanzaCoreConfig } from '@getstanza/core'
+import type { init } from '@getstanza/node'
+type NodeConfig = Parameters<typeof init>[0]
 
-const stanzaKey = process.env.NEXT_PUBLIC_STANZA_BROWSER_KEY
+const stanzaBrowserKey = process.env.NEXT_PUBLIC_STANZA_BROWSER_KEY
 
-if (typeof stanzaKey !== 'string') {
-  const s = 'The NEXT_PUBLIC_STANZA_BROWSER_KEY environment variable has not been set. It must be set to a valid key at build time for this sample application to work correctly.'
-  console.error('- \x1b[31;1merror\x1b[0m ' + s) // Ansi code for red+bold to fit with NX log styling. (Alternatively, could use chalk library.)
+if (typeof stanzaBrowserKey !== 'string') {
+  throw new Error('The NEXT_PUBLIC_STANZA_BROWSER_KEY environment variable has not been set. It must be set to a valid key at build time for this sample application to work correctly.')
 }
 
-export const config: StanzaCoreConfig = {
-  url: process.env.NEXT_PUBLIC_STANZA_HUB_ADDRESS ?? 'https://hub.stanzasys.co',
-  environment: process.env.NEXT_PUBLIC_STANZA_ENVIRONMENT ?? 'local',
-  stanzaApiKey: stanzaKey ?? '',
+const stanzaApiKey = process.env.NEXT_PUBLIC_STANZA_API_KEY
+
+if (typeof stanzaApiKey !== 'string') {
+  throw new Error('NEXT_PUBLIC_STANZA_API_KEY is a required environment variable')
+}
+
+const hubUrl = process.env.NEXT_PUBLIC_STANZA_HUB_ADDRESS ?? 'https://hub.stanzasys.co'
+const environment = process.env.NEXT_PUBLIC_STANZA_ENVIRONMENT ?? 'local'
+export const browserConfig = {
+  url: hubUrl,
+  environment,
+  stanzaApiKey: stanzaBrowserKey,
   contextConfigs: [
     {
       name: 'main',
@@ -25,5 +34,14 @@ export const config: StanzaCoreConfig = {
     }
     return 100
   }
+} satisfies StanzaCoreConfig
 
-}
+export const nodeConfig = {
+  hubUrl,
+  environment,
+  apiKey: stanzaApiKey,
+  serviceName: 'DemoCommerce',
+  serviceRelease: '1',
+  useRestHubApi: true,
+  requestTimeout: 2000
+} satisfies NodeConfig
