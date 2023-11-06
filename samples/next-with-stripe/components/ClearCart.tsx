32187719f0fd1631fbe493d52e5d19eb15919991
sre-is-carlos <carlos@sre.is>
2023-11-06 16:47:10 -0300

    fix: fix result page clearCart function infinite looping

--- samples/next-with-stripe/components/ClearCart.tsx
+++ samples/next-with-stripe/components/ClearCart.tsx
@@ -1,10 +1,16 @@
-import { useEffect } from 'react'
+import { useCallback, useEffect } from 'react'
 import { useShoppingCart } from 'use-shopping-cart'
 
 export default function ClearCart () {
   const { clearCart } = useShoppingCart()
 
-  useEffect(() => { clearCart() }, [clearCart])
+  const clearCartItems = useCallback(() => {
+    return clearCart
+  }, [clearCart])
+
+  useEffect(() => {
+    clearCartItems()
+  }, [clearCartItems])
 
   return <p>Cart cleared.</p>
 }
