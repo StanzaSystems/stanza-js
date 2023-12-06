2db01f883cbb0328c078ab79130eb35197bc3ec2
John A <147433179+johnsre@users.noreply.github.com>
2023-12-06 18:10:30 -0300

    chore: update react code and StanzaConfiguredSampler

--- samples/next-with-stripe/components/WithStanza.tsx
+++ samples/next-with-stripe/components/WithStanza.tsx
@@ -1,11 +1,38 @@
 'use client';
-import React, { type PropsWithChildren } from 'react';
-import { createStanzaInstance, StanzaProvider } from '@getstanza/react';
+import React, {
+  useState,
+  type PropsWithChildren,
+  useCallback,
+  useEffect,
+} from 'react';
+import {
+  createStanzaInstance,
+  type StanzaInstance,
+  StanzaProvider,
+} from '@getstanza/react';
 import { browserConfig } from '../stanzaConfig';
 
-const stanzaInstance = await createStanzaInstance(browserConfig);
-
 const WithStanza: React.FC<PropsWithChildren> = ({ children }) => {
+  const [stanzaInstance, setStanzaInstance] = useState<StanzaInstance | null>(
+    null
+  );
+
+  const getStanzaConfig = useCallback(async () => {
+    const stanzaInstance = await createStanzaInstance(browserConfig);
+
+    setStanzaInstance(stanzaInstance);
+  }, []);
+
+  useEffect(() => {
+    if (stanzaInstance === null) {
+      void getStanzaConfig();
+    }
+  }, [getStanzaConfig, stanzaInstance]);
+
+  if (stanzaInstance === null) {
+    return null;
+  }
+
   return <StanzaProvider instance={stanzaInstance}>{children}</StanzaProvider>;
 };
 
--- samples/simple-next/src/pages/_app.tsx
+++ samples/simple-next/src/pages/_app.tsx
@@ -1,24 +1,47 @@
-import { createStanzaInstance, StanzaProvider } from '@getstanza/react';
+import {
+  createStanzaInstance,
+  type StanzaInstance,
+  StanzaProvider,
+} from '@getstanza/react';
 import type { AppProps } from 'next/app';
 import * as process from 'process';
 import { browserConfig } from '../stanzaConfig';
 import '../styles/globals.css';
+import { useCallback, useEffect, useState } from 'react';
 
 let loadPromise: Promise<any> = Promise.resolve();
 if (process.env.NODE_ENV === 'development') {
   const mswMock = import('../msw/mock');
   loadPromise = mswMock.then(async (module) => module.initMocks());
 }
-if (!process.browser) {
+if (typeof window === 'undefined') {
   loadPromise = new Promise(() => {});
 }
 
-const stanzaInstance = await createStanzaInstance({
-  ...browserConfig,
-  pollDelay: loadPromise,
-});
-
 function MyApp({ Component, pageProps }: AppProps) {
+  const [stanzaInstance, setStanzaInstance] = useState<StanzaInstance | null>(
+    null
+  );
+
+  const getStanzaConfig = useCallback(async () => {
+    const stanzaInstance = await createStanzaInstance({
+      ...browserConfig,
+      pollDelay: loadPromise,
+    });
+
+    setStanzaInstance(stanzaInstance);
+  }, []);
+
+  useEffect(() => {
+    if (stanzaInstance === null) {
+      void getStanzaConfig();
+    }
+  }, [getStanzaConfig, stanzaInstance]);
+
+  if (stanzaInstance === null) {
+    return null;
+  }
+
   return (
     <StanzaProvider instance={stanzaInstance}>
       <Component {...pageProps} />
