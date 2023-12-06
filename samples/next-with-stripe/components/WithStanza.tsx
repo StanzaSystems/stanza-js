cab299a64ab136cf2b36c6627d96aa9e2d0a758f
John A <147433179+johnsre@users.noreply.github.com>
2023-12-06 17:16:12 -0300

    chore: updating nextjs sample and tsconfig file

--- samples/next-with-stripe/components/WithStanza.tsx
+++ samples/next-with-stripe/components/WithStanza.tsx
@@ -3,7 +3,7 @@ import React, { type PropsWithChildren } from 'react';
 import { createStanzaInstance, StanzaProvider } from '@getstanza/react';
 import { browserConfig } from '../stanzaConfig';
 
-const stanzaInstance = createStanzaInstance(browserConfig);
+const stanzaInstance = await createStanzaInstance(browserConfig);
 
 const WithStanza: React.FC<PropsWithChildren> = ({ children }) => {
   return <StanzaProvider instance={stanzaInstance}>{children}</StanzaProvider>;
