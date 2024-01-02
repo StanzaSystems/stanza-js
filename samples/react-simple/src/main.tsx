b00f084a06eb0c1cff24d45d48045ccb642b9b8a
John A <147433179+johnsre@users.noreply.github.com>
2024-01-02 14:33:49 -0300

    chore: fixing createStanzaInstance in samples

--- samples/react-simple/src/main.tsx
+++ samples/react-simple/src/main.tsx
@@ -15,7 +15,7 @@ if (import.meta.env.MODE === 'development') {
 
 await loadPromise;
 
-const stanzaInstance = await createStanzaInstance(config);
+const stanzaInstance = createStanzaInstance(config);
 
 const rootElementId = 'root';
 const rootElement = document.getElementById(rootElementId);
--- samples/simple-next/src/pages/_app.tsx
+++ samples/simple-next/src/pages/_app.tsx
@@ -1,13 +1,8 @@
-import {
-  createStanzaInstance,
-  type StanzaInstance,
-  StanzaProvider,
-} from '@getstanza/react';
+import { createStanzaInstance, StanzaProvider } from '@getstanza/react';
 import type { AppProps } from 'next/app';
 import * as process from 'process';
 import { browserConfig } from '../stanzaConfig';
 import '../styles/globals.css';
-import { useCallback, useEffect, useState } from 'react';
 
 let loadPromise: Promise<any> = Promise.resolve();
 if (process.env.NODE_ENV === 'development') {
@@ -18,30 +13,12 @@ if (typeof window === 'undefined') {
   loadPromise = new Promise(() => {});
 }
 
+const stanzaInstance = createStanzaInstance({
+  ...browserConfig,
+  pollDelay: loadPromise,
+});
+
 function MyApp({ Component, pageProps }: AppProps) {
-  const [stanzaInstance, setStanzaInstance] = useState<StanzaInstance | null>(
-    null
-  );
-
-  const getStanzaConfig = useCallback(async () => {
-    const stanzaInstance = await createStanzaInstance({
-      ...browserConfig,
-      pollDelay: loadPromise,
-    });
-
-    setStanzaInstance(stanzaInstance);
-  }, []);
-
-  useEffect(() => {
-    if (stanzaInstance === null) {
-      void getStanzaConfig();
-    }
-  }, [getStanzaConfig, stanzaInstance]);
-
-  if (stanzaInstance === null) {
-    return null;
-  }
-
   return (
     <StanzaProvider instance={stanzaInstance}>
       <Component {...pageProps} />
